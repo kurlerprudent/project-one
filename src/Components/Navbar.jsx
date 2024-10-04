902b5424e164c4f5cac65a411a9cccda3ee6df40
kurlerprudent <osarkodie@st.ug.edu.gh>
2024-10-04 14:55:19 +0000

    Update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,10 +3,16 @@ import React, { useEffect, useState } from 'react';
 import logo from '../Images/logo.png';  
 import MenuIcon from '@mui/icons-material/Menu';  
 import './Navbar.css';  
+import { Close } from '@mui/icons-material';
 
 const Navbar = () => {  
   const [isScrollDown, setIsScrollDown] = useState(false);  
-   
+  const [isOpen, setisOpen] = useState(false) ;
+
+  const handleClick = ()=>{
+    setisOpen(!isOpen)
+  }
+
   useEffect(() => {  
     let prevScrollPosition = window.pageYOffset
     const handleScroll = () => {  
@@ -23,14 +29,47 @@ const Navbar = () => {
   }, []);  
 
   return (  
+    <>
     <AppBar sx={{bgcolor:'transparent'}} className={`navbar ${isScrollDown ? 'scrolled' : ''}`}>  
       <Toolbar className="nav-container">  
-        <Box component="img" src={logo} alt="logo" sx={{ height: 40 }} />  
-        <IconButton edge="end" color="inherit" aria-label="menu">  
-          <MenuIcon sx={{ color: 'white', fontSize: 40 }} />  
-        </IconButton>  
-      </Toolbar>  
+        <div className='logo'>
+        <Box  component="img" src={logo} alt="logo" sx={{ height: 40 }} /> 
+        </div>
+         
+
+         <div className='nav-links'>
+            <ul className='links'>
+                <li className="link">Home</li>
+                <li className="link">About</li>
+                <li className="link">Schools</li>
+                <li className="link">Contact</li>
+                <li className="link">Student Menu</li>
+            </ul>
+         </div>
+      </Toolbar> 
+   
+      <div className='menu-icon'>
+      <IconButton  onClick={handleClick} edge="end" color="inherit" aria-label="menu"> 
+        {
+            isOpen ? (<div className="mobile-nav">
+                <Close sx={{color:'white', position:'absolute',top:5,right:10,fontSize:40}}/>
+                <div className='mobile-nav-links'>
+                    <ul>
+                        <li className="link">Home</li>
+                        <li className="link">About</li>
+                        <li className="link">Schools</li>
+                        <li className="link">Contact</li>
+                        <li className="link">Student Menu</li>
+                    </ul>
+                </div>
+            </div>) : ( <MenuIcon sx={{ color: 'white', fontSize: 40 }} />  )
+        } 
+         
+        </IconButton> 
+      </div>
     </AppBar>  
+    
+    </>
   );  
 };  
 
